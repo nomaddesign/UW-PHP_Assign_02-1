a8f47b659df637124fbd154175e198aefc8aa081
nomaddesign <nomaddesign@gmail.com>
2013-02-12 22:19:02 -0800

    PDO fixes

--- Src/db/jDBAL.php
+++ Src/db/jDBAL.php
@@ -4,7 +4,7 @@ namespace db;
 
 use \PDO as PDO;
 
-class jDBAL {
+class jDBAL extends PDO {
 	
 	private $hostname;
 	private $database;
@@ -13,7 +13,7 @@ class jDBAL {
 	private $rows;
 	public $result;
 	
-	public function __construct($hostname, $username, $password, $database) {
+	public function __construct($hostname, $database, $username, $password) {
 		$this->hostname = $hostname;
 		$this->database = $database;
 		$this->username = $username;
@@ -21,36 +21,81 @@ class jDBAL {
 	}
 	
 	// connect to the database
-	public function connect(){     
+	public function connect(){
+		try {
+			# MySQL with PDO_MYSQL  
+			$this->dbh = new PDO("mysql:host=$this->hostname;dbname=$this->database", $this->username, $this->password );
+				
+			#Set Error Mode
+			$this->dbh->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING );
+				
+		} catch(\RuntimeException $e) {
+		    die( 'Failed to establish database connection, error:'. $e->getMessage() );
+		}// END Try/Catch	  
+	}
 	
-		try {    
+	// return the result of a select query. This function may return an array of rows, an array of models that represent the tables or rows.
+	public function select($query) {
+		//SetUp Array for results
+		$results = array();
 		
-		  # MySQL with PDO_MYSQL  
-		  $DBH = new PDO("mysql:host=$this->hostname;dbname=$this->database", $this->username, $this->password );  
-		  
-		  #Set Error Mode
-		  $DBH->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING ); 
-		}  
-		catch(PDOException $e) {  
-		    echo $e->getMessage();  
-		}  
+		try {
+			$statement = $this->dbh->prepare($query);//Build PDO Statement
+			$statement->execute();//execute
+			$statement->setFetchMode(PDO::FETCH_ASSOC); 
+			//Loop through fetched rows
+			while ($row = $statement->fetch()){
+				array_push($results, $row);
+			}
+		} catch(\RuntimeException $e) {
+		    die( 'Failed to select record(s):'. $e->getMessage() );
+		}// END Try/Catch
+		
+		return $results; 
 	}
 	
-	public function query() {      // return the result of a select query. This function may return an array of rows, an array of models that represent the tables or rows.
+	// execute the update sql statement. It should throw an exception if the query fails to execute.
+	public function update($query) {
+		try {
+			$updateResult = $this->dbh->exec($query);
+		} catch(\RuntimeException $e) {
+		    die( 'Failed to update record:'. $e->getMessage() );
+		}// END Try/Catch
+		
+		return $updateResult;
 	}
 	
-	public function update() {     // execute the update sql statement. It should throw an exception if the query fails to execute.
+	// execute the delete sql statement. It should throw an exception if the query fails to execute.
+	public function delete($query) {
+		try {
+			$deleteResult = $this->dbh->exec($query);
+		} catch(\RuntimeException $e) {
+		    die( 'Failed to delete record:'. $e->getMessage() );
+		}// END Try/Catch
+		
+		return $deleteResult; 
 	}
 	
-	public function delete() {     // execute the delete sql statement. It should throw an exception if the query fails to execute.
+	// execute insert sql statement. It should return last insert id if operation is successful, or throw an exception if the operation fails.
+	public function insert($query) {
+		try {
+			$num = $this->dbh->exec($query);
+			$insertResultID = $this->dbh->lastInsertId();
+		} catch(\RuntimeException $e) {
+	    	die( 'Failed to update record:'. $e->getMessage() );
+		}// END Try/Catch
+		
+		return $insertResultID;
 	}
 	
-	public function insert() {     // execute insert sql statement. It should return last insert id if operation is successful, or throw an exception if the operation fails.
-	}
-	
-	public function disconnect(){  // shutdowns established database connection
+	// shutdowns established database connection
+	public function disconnect(){  
 		# close the connection
-		$DBH = null;
+		try {
+			$this->dbh = null;
+		} catch(\RuntimeException $e) {
+		    die( 'Failed to close connection:'. $e->getMessage() );
+		}
 	}
 	  
 }
--- Src/index.php
+++ Src/index.php
@@ -1,27 +1,50 @@
 <?php
 
-use Db\Connection;
+namespace db;
 
-$db = new Connection( array(
-                'host'     => 'localhost',
-                'username' => 'foo',
-                'password' => 'password',
-                'dbname'   => 'mydatabase'
-            ) );
+
+
+$e = null;
+
+error_reporting( E_ALL | E_STRICT );
+ini_set('display_errors','On');
+ini_set('display_startup_errors','On');
+
+require_once ('../Bootstrap.php');
+
+require_once ('db/jDBAL.php');
+
+echo " Hello World";
+
+$db = new jDBAL('127.0.0.1','assn2','mcdata','ci5ku6zu0show');
 
 // connect to database, expect to
 try {
     $db->connect();
 } catch(\RuntimeException $e) {
     die( 'fails to establish database connection, error:'. $e->getMessage() );
+    
 }
 
-$insertSql = "'INSERT INTO Post (firstname, lastname) VALUES ('John', 'Smith')";
+echo $e;
+
+//$db->disconnect();
+
+$insertSql = "INSERT INTO User (firstname, lastname) VALUES ('Bob', 'Smith')";
 $rowId = $db->insert($insertSql);
 
-$newRow = $db->select( sprintf("SELECT * FROM User WHERE id = %d", $rowId ) );
+echo $rowId;
+
+$id = array(5,7,9,11,13); 
+
+$newRow = $db->select(sprintf("SELECT * FROM User WHERE id = %d", $rowId ));
+
+\var_dump ($newRow);
 
 // Should have at least one row
 assert(count($newRow) > 0);
+
+
+
 
 ?>
